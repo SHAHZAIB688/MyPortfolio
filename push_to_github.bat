fd61831cc51d258fbfde3df15604c3335f7dc747
SHAHZAIB688 <malikshahzaib688343@gmail.com>
2026-03-22 15:28:36 +0500

    Refactor: Simplify push script by removing repository initialization and remote setup, and updating commit and push commands.

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,8 +1,4 @@
 @echo off
-git init
 git add .
-git commit -m "Deploy: Portfolio Completed"
-git branch -M main
-git remote remove origin
-git remote add origin https://github.com/SHAHZAIB688/MyPortfolio.git
-git push -u origin main --force
+git commit -m "Fix: Cloud Microservice Architecture & Vercel Linking"
+git push
